90d44909fe460dec3f480062ae7f61ccfb095aaa
Uladzislau Blok <blokv75@gmail.com>
2025-11-29 14:14:33 +0100

    Minimum Operations to Make Array Sum Divisible by K

---
+++ java/app/src/main/java/org/bloku/task/_3512/Solution.java
@@ -0,0 +1,17 @@
+package org.bloku.task._3512;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Minimum Operations to Make Array Sum Divisible by K")
+@Topics({ARRAY, MATH})
+class Solution {
+
+    public int minOperations(int[] nums, int k) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        return sum % k;
+    }
+}
