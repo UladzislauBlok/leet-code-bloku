9343b10d8116653df5cf36d305d83a58003ea2c5
Uladzislau Blok <blokv75@gmail.com>
2025-12-07 13:21:24 +0100

    Count Odd Numbers in an Interval Range

---
+++ java/app/src/main/java/org/bloku/task/_1523/Solution.java
@@ -0,0 +1,18 @@
+package org.bloku.task._1523;
+
+import static org.bloku.util.Topic.MATH;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Count Odd Numbers in an Interval Range")
+@Topics({MATH})
+class Solution {
+
+    public int countOdds(int low, int high) {
+        if (low == high) return low % 2;
+        low = low % 2 == 0 ? low + 1 : low;
+        high = high % 2 == 0 ? high - 1 : high;
+        return (high - low + 1) / 2 + 1;
+    }
+}
---
+++ java/app/src/test/java/org/bloku/task/_1523/SolutionTest.java
@@ -0,0 +1,36 @@
+package org.bloku.task._1523;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solutionReturnsExpectedResult(int low, int high, int expected) {
+        // given
+
+        // when
+        int actual = solution.countOdds(low, high);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> solutionReturnsExpectedResult() {
+        return Stream.of(Arguments.of(3, 7, 3), Arguments.of(8, 10, 1));
+    }
+}
