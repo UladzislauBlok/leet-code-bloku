3fa0442b451d2b53d525c8175ef2884f0963626a
Uladzislau Blok <blokv75@gmail.com>
2025-12-09 23:20:32 +0100

    Count Special Triplets

---
+++ java/app/src/test/java/org/bloku/task/_3583/SolutionTest.java
@@ -0,0 +1,37 @@
+package org.bloku.task._3583;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solutionReturnsExpectedResult(int[] nums, int expected) {
+        // given
+
+        // when
+        int actual = solution.specialTriplets(nums);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> solutionReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of(new int[] {6, 3, 6}, 1), Arguments.of(new int[] {8, 4, 2, 8, 4}, 2));
+    }
+}
