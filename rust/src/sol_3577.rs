60f91eb86403da008f0c19eb5e8626b528864bfd
Uladzislau Blok <blokv75@gmail.com>
2025-12-10 22:39:30 +0100

    Count the Number of Computer Unlocking Permutations: rust

---
+++ rust/src/sol_3577.rs
@@ -0,0 +1,18 @@
+struct Solution {}
+
+impl Solution {
+    pub fn count_permutations(complexity: Vec<i32>) -> i32 {
+        const MOD: u64 = 1_000_000_007;
+        let n = complexity.len();
+        for i in 1..n {
+            if (complexity[i] <= complexity[0]) {
+                return 0;
+            }
+        }
+        let mut res: u64 = 1;
+        for i in 2..n {
+            res = (res * i as u64) % MOD;
+        }
+        (res % MOD) as i32
+    }
+}
