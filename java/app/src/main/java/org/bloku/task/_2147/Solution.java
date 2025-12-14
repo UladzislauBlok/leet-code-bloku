924cfd6044666059dad39d36448f0a527a9845f8
Uladzislau Blok <blokv75@gmail.com>
2025-12-14 22:15:05 +0100

    Number of Ways to Divide a Long Corridor

---
+++ java/app/src/main/java/org/bloku/task/_2147/Solution.java
@@ -0,0 +1,51 @@
+package org.bloku.task._2147;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Number of Ways to Divide a Long Corridor")
+@Topics({MATH, STRING, DYNAMIC_PROGRAMMING})
+class Solution {
+
+    private static final int MOD = (int) 1e9 + 7;
+
+    public int numberOfWays_(String corridor) {
+        return dp(corridor.toCharArray(), 0);
+    }
+
+    public int numberOfWays(String corridor) {
+        long result = 1;
+        int prevSeatIndex = 0, n = corridor.length();
+        int numSeats = 0;
+        char[] chars = corridor.toCharArray();
+
+        for (int i = 0; i < n; i++) {
+            char c = chars[i];
+            if (c == 'S') {
+                numSeats++;
+                if (numSeats > 2 && numSeats % 2 == 1) {
+                    result = result * (i - prevSeatIndex) % MOD;
+                }
+                prevSeatIndex = i;
+            }
+        }
+
+        return numSeats > 1 && numSeats % 2 == 0 ? (int) result : 0;
+    }
+
+    private int dp(char[] corridor, int pos) {
+        int s = 0, n = corridor.length;
+        while (pos < n && s < 2) {
+            if (corridor[pos] == 'S') s++;
+            pos++;
+        }
+        if (s < 2) return 0;
+        int p = pos;
+        while (pos < n && corridor[pos] != 'S') pos++;
+        if (pos == n) return 1;
+        int res = dp(corridor, pos) % MOD;
+        return (int) (((pos - p + 1L) * res) % MOD);
+    }
+}
---
+++ java/app/src/test/java/org/bloku/task/_2147/SolutionTest.java
@@ -0,0 +1,37 @@
+package org.bloku.task._2147;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solutionReturnsExpectedResult(String corridor, int expected) {
+        // given
+
+        // when
+        int actual = solution.numberOfWays(corridor);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> solutionReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of("SSPPSPS", 3), Arguments.of("SSPPSPSSPPPSSPSPPSSPSPSPPSS", 54));
+    }
+}
