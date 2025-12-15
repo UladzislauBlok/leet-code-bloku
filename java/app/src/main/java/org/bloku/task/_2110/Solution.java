f3ba12bdaccde13920e8a7446eda37c368ce3fa4
Uladzislau Blok <blokv75@gmail.com>
2025-12-15 19:58:32 +0100

    Number of Smooth Descent Periods of a Stock

---
+++ java/app/src/main/java/org/bloku/task/_2110/Solution.java
@@ -0,0 +1,26 @@
+package org.bloku.task._2110;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+@LeetCodeName("Number of Smooth Descent Periods of a Stock")
+@Topics({ARRAY, MATH, DYNAMIC_PROGRAMMING})
+class Solution {
+
+    public long getDescentPeriods(int[] prices) {
+        long res = 0;
+        int prev = -1, window = 0;
+        for (int price : prices) {
+            if (prev - 1 == price) {
+                window++;
+            } else {
+                window = 1;
+            }
+            res += window;
+            prev = price;
+        }
+        return res;
+    }
+}
---
+++ java/app/src/test/java/org/bloku/task/_2110/SolutionTest.java
@@ -0,0 +1,37 @@
+package org.bloku.task._2110;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solutionReturnsExpectedResult(int[] prices, long expected) {
+        // given
+
+        // when
+        long actual = solution.getDescentPeriods(prices);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> solutionReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of(new int[] {3, 2, 1, 4}, 7), Arguments.of(new int[] {8, 6, 7, 7}, 4));
+    }
+}
