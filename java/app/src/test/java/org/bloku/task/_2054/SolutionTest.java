c140a04d12c2226d464794fc688cf42b72971469
Uladzislau Blok <blokv75@gmail.com>
2025-12-23 11:47:56 +0100

    Two Best Non-Overlapping Events

---
+++ java/app/src/test/java/org/bloku/task/_2054/SolutionTest.java
@@ -0,0 +1,38 @@
+package org.bloku.task._2054;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solutionReturnsExpectedResult(int[][] events, int expected) {
+        // given
+
+        // when
+        int actual = solution.maxTwoEvents(events);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> solutionReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of(new int[][] {{1, 3, 2}, {4, 5, 2}, {2, 4, 3}}, 4),
+                Arguments.of(new int[][] {{1, 3, 2}, {4, 5, 2}, {1, 5, 5}}, 5));
+    }
+}
---
+++ java/app/src/test/java/org/bloku/task/_960/SolutionTest.java
@@ -0,0 +1,38 @@
+package org.bloku.task._960;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solutionReturnsExpectedResult(String[] strs, int expected) {
+        // given
+
+        // when
+        int actual = solution.minDeletionSize(strs);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> solutionReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of(new String[] {"babca", "bbazb"}, 3),
+                Arguments.of(new String[] {"ghi", "def", "abc"}, 0));
+    }
+}
