c48f81aa66c384acdc66b39cd1a149cb61f9f56c
Uladzislau Blok <blokv75@gmail.com>
2025-12-24 14:58:52 +0100

    Apple Redistribution into Boxes

---
+++ java/app/src/main/java/org/bloku/task/_3074/Solution.java
@@ -0,0 +1,27 @@
+package org.bloku.task._3074;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+import java.util.Arrays;
+
+@LeetCodeName("Apple Redistribution into Boxes")
+@Topics({ARRAY, GREEDY, SORTING, PREFIX_SUM})
+class Solution {
+
+    public int minimumBoxes(int[] apples, int[] capacity) {
+        Arrays.sort(capacity);
+        int sum = 0;
+        for (int apple : apples) sum += apple;
+        int prefix = 0, n = capacity.length;
+        for (int i = n - 1; i >= 0; i--) {
+            prefix += capacity[i];
+            if (sum <= prefix) {
+                return n - i;
+            }
+        }
+        return -1; // never the case because of constraints
+    }
+}
---
+++ java/app/src/test/java/org/bloku/task/_3074/SolutionTest.java
@@ -0,0 +1,38 @@
+package org.bloku.task._3074;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solutionReturnsExpectedResult(int[] apples, int[] capacity, int expected) {
+        // given
+
+        // when
+        int actual = solution.minimumBoxes(apples, capacity);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> solutionReturnsExpectedResult() {
+        return Stream.of(
+                Arguments.of(new int[] {1, 3, 2}, new int[] {4, 3, 1, 5, 2}, 2),
+                Arguments.of(new int[] {5, 5, 5}, new int[] {2, 4, 2, 7}, 4));
+    }
+}
