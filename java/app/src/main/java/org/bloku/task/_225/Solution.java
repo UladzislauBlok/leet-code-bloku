863cc9976c87d3b093694e306e01900659cc7a95
Uladzislau Blok <blokv75@gmail.com>
2025-12-26 14:50:11 +0100

    Implement Stack using Queues

---
+++ java/app/src/main/java/org/bloku/task/_225/Solution.java
@@ -0,0 +1,48 @@
+package org.bloku.task._225;
+
+import static org.bloku.util.Topic.*;
+
+import org.bloku.util.LeetCodeName;
+import org.bloku.util.Topics;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+@LeetCodeName("Implement Stack using Queues")
+@Topics({DESIGN, STACK, QUEUE})
+class Solution {
+
+    static class MyStack {
+
+        private final Deque<Integer> queue = new ArrayDeque<>();
+        private final Deque<Integer> stack = new ArrayDeque<>();
+
+        public MyStack() {}
+
+        public void push(int x) {
+            stack.add(x);
+        }
+
+        public int pop() {
+            if (stack.isEmpty() || stack.size() > 1) move();
+            if (stack.isEmpty()) return -1;
+            return stack.poll();
+        }
+
+        public int top() {
+            if (stack.isEmpty() || stack.size() > 1) move();
+            if (stack.isEmpty()) return -1;
+            return stack.peek();
+        }
+
+        public boolean empty() {
+            return queue.isEmpty() && stack.isEmpty();
+        }
+
+        private void move() {
+            while (!stack.isEmpty()) queue.add(stack.poll());
+            while (!queue.isEmpty()) stack.add(queue.poll());
+            while (stack.size() > 1) queue.add(stack.poll());
+        }
+    }
+}
---
+++ java/app/src/test/java/org/bloku/task/_225/SolutionTest.java
@@ -0,0 +1,26 @@
+package org.bloku.task._225;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution.MyStack sut;
+
+    @BeforeEach
+    public void setUp() {
+        this.sut = new Solution.MyStack();
+    }
+
+    @Test
+    void tc1() {
+        sut.push(1);
+        sut.push(2);
+
+        assertThat(sut.top()).isEqualTo(2);
+        assertThat(sut.pop()).isEqualTo(2);
+        assertThat(sut.empty()).isFalse();
+    }
+}
