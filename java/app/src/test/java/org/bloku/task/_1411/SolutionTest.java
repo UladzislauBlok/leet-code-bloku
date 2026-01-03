ab7dc4305121d9d54307c7a6fbd95d103dee97e6
Uladzislau Blok <blokv75@gmail.com>
2026-01-03 23:49:34 +0100

    Number of Ways to Paint N × 3 Grid

---
+++ java/app/src/test/java/org/bloku/task/_1411/SolutionTest.java
@@ -0,0 +1,36 @@
+package org.bloku.task._1411;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        this.solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void solutionReturnsExpectedResult(int n, int expected) {
+        // given
+
+        // when
+        int actual = solution.numOfWays(n);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> solutionReturnsExpectedResult() {
+        return Stream.of(Arguments.of(2, 54), Arguments.of(5000, 30228214));
+    }
+}
