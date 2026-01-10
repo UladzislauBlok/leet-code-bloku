9b0af0f34bc559839fd90539c9b39e3138bae8ff
Uladzislau Blok <blokv75@gmail.com>
2026-01-10 16:01:01 +0100

    Minimum ASCII Delete Sum for Two Strings: iter

--- java/app/src/main/java/org/bloku/task/_712/Solution.java
+++ java/app/src/main/java/org/bloku/task/_712/Solution.java
@@ -12,6 +12,27 @@ import java.util.Map;
 @Topics({STRING, DYNAMIC_PROGRAMMING})
 class Solution {
 
+    public int minimumDeleteSum(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        for (int i = 0; i < n1; i++) dp[i + 1][0] = dp[i][0] + s1.charAt(i);
+        for (int i = 0; i < n2; i++) dp[0][i + 1] = dp[0][i] + s2.charAt(i);
+        for (int i = 0; i < n1; i++) {
+            char c1 = s1.charAt(i);
+            for (int j = 0; j < n2; j++) {
+                char c2 = s2.charAt(j);
+                if (c1 == c2) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else {
+                    // copy prev result + remove c1 or c2
+                    dp[i + 1][j + 1] = Math.min(dp[i][j + 1] + c1, dp[i + 1][j] + c2);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+
     private final Map<Integer, Integer> memo = new HashMap<>();
 
     private String s1;
@@ -19,7 +40,7 @@ class Solution {
     private String s2;
     private int n2;
 
-    public int minimumDeleteSum(String s1, String s2) {
+    public int minimumDeleteSumRecursion(String s1, String s2) {
         this.s1 = s1;
         this.n1 = s1.length();
         this.s2 = s2;
