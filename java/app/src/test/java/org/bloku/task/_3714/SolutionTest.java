75b5ec7b8ca4b71d2378513de88129534ce906a2
Uladzislau Blok <blokv75@gmail.com>
2026-02-13 22:19:03 +0100

    Longest Balanced Substring II

---
+++ java/app/src/test/java/org/bloku/task/_3714/SolutionTest.java
@@ -0,0 +1,35 @@
+package org.bloku.task._3714;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SolutionTest {
+
+  private Solution solution;
+
+  @BeforeEach
+  public void setUp() {
+    this.solution = new Solution();
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void solutionReturnsExpectedResult(String s, int expected) {
+    // given
+
+    // when
+    int actual = solution.longestBalanced(s);
+
+    // then
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> solutionReturnsExpectedResult() {
+    return Stream.of(Arguments.of("abbac", 4), Arguments.of("aabcc", 3));
+  }
+}
